2122d5516da7aac414c38e4f6fad7e98584ead98
Infatzmut <54115042+Infatzmut@users.noreply.github.com>
2023-04-07 15:08:46 -0500

    fixed deployment script

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 sh "sed -i 's#replace#${imageName}#g' k8s_deployment_service.yaml"
-kubectl -n default get deployment ${deployment} > /dev/null
+# kubectl -n default get deployment ${deployment} > /dev/null
 
-if [[ $? -ne 0 ]]; then
-    echo "deployment ${deploymentName} does not exist"
-    sh "kubectl -n default apply -f k8s_deployment_service.yaml"
-else
-    echo "deployment ${deploymentName} exist"
-    echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi
+# if [[ $? -ne 0 ]]; then
+#     echo "deployment ${deploymentName} does not exist"
+#     sh "kubectl -n default apply -f k8s_deployment_service.yaml"
+# else
+#     echo "deployment ${deploymentName} exist"
+#     echo "image name - ${imageName}"
+#     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+# fi
+
+sh "kubectl -n default apply -f k8s_deployment_service.yaml"
